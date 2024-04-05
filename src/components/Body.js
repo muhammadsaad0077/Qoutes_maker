4364ff248bd687de155bce1c906d3a4f2ef4af65
muhammadsaad0077 <saaditanoli12@gmail.com>
2024-04-05 15:56:11 +0500

    planning and project setup

---
+++ src/components/Body.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+    <div className='bg-blue-500 w-10'>
+        <div className='bg-white text-blue-500'>
+            
+        </div>
+      
+    </div>
+  )
+}
+
+export default Body
